15d770e8800a13913639ae271906cf8268f6b6c2
xzy <15271131814@163.com>
2025-12-17 15:29:00 +0800

    简单使用（只有查询user表 全部内容）

---
+++ src/main/java/com/mybatis/MybatisDemo.java
@@ -0,0 +1,31 @@
+package com.mybatis;
+
+import com.mybatis.mapper.UserMapper;
+import com.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MybatisDemo {
+    public static void main(String[] args) {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> list=userMapper.getUser();
+        for(User user:list){
+            System.out.println(user);
+        }
+    }
+}
---
+++ src/main/java/com/mybatis/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.mybatis.mapper;
+import com.pojo.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    List<User> getUser();
+}
